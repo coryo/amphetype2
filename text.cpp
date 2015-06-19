cacde31bb57d980a37a8f421ce78247d1570d182
Cory Parsons <babylonstudio@gmail.com>
2015-06-18 22:39:46 -0400

    added more values

--- text.cpp
+++ text.cpp
@@ -4,18 +4,23 @@
 #include <QString>
 
 Text::Text(const QByteArray& id, int source, const QString& text)
-        : id(new QByteArray(id)), source(source), text(new QString(text))
-{
-}
+        : id(id), source(source), text(text), sourceName(""), textNumber(0)
+{}
+
+Text::Text(const QByteArray& id, int source, const QString& text, const QString& sName, int tNum)
+        : id(id), source(source), text(text), sourceName(sName), textNumber(tNum)
+{}
 
 Text::~Text()
 {
-        delete id;
-        delete text;
 }
 
-const QByteArray& Text::getId() const { return *id; }
+const QByteArray& Text::getId() const { return id; }
 
 int Text::getSource() const { return source; }
 
-const QString& Text::getText() const { return *text; }
+const QString& Text::getText() const { return text; }
+
+const QString& Text::getSourceName() const { return sourceName; }
+
+int Text::getTextNumber() const { return textNumber; }
--- text.h
+++ text.h
@@ -1,21 +1,26 @@
 #ifndef TEXT_H
 #define TEXT_H
 
-class QByteArray;
-class QString;
+#include <QByteArray>
+#include <QString>
 
 class Text {
 public:
         Text(const QByteArray&, int, const QString&);
+        Text(const QByteArray&, int, const QString&, const QString&, int);
         ~Text();
         const QByteArray& getId() const;
         int getSource() const;
         const QString& getText() const;
+        const QString& getSourceName() const;
+        int getTextNumber() const;
 
 private:
-        QByteArray* id;
+        QByteArray id;
         int source;
-        QString* text;
+        QString text;
+        QString sourceName;
+        int textNumber;
 };
 
 #endif // TEXT_H
