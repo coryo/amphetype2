9cb085e53b809c6427c4dc2846763af6a388a7d6
Cory Parsons <babylonstudio@gmail.com>
2016-09-29 01:39:05 -0400

    add source model

---
+++ src/texts/sourcemodel.cpp
@@ -0,0 +1,202 @@
+#include "texts/sourcemodel.h"
+
+#include <QsLog.h>
+
+#include "database/db.h"
+
+SourceItem::SourceItem(int id, const QString& name, int disabled, int discount,
+                       int type, int count, int results, double wpm)
+    : id_(id),
+      name_(name),
+      disabled_(disabled),
+      discount_(discount),
+      type_(type),
+      text_count_(count),
+      results_(results),
+      wpm_(wpm) {}
+
+void SourceItem::refresh() {
+  Database db;
+  auto data = db.getSourceData(id_);
+  id_ = data[0].toInt();
+  name_ = data[1].toString();
+  disabled_ = data[5].isNull() ? 1 : 0;
+  discount_ = 0;
+  type_ = 0;
+  text_count_ = data[2].toInt();
+  results_ = data[3].toInt();
+  wpm_ = data[4].toDouble();
+}
+
+void SourceItem::deleteFromDb() {
+  Database db;
+  QList<int> ids;
+  ids << id_;
+  db.deleteSource(ids);
+}
+
+void SourceItem::enable() {
+  Database db;
+  QList<int> ids;
+  ids << id_;
+  db.enableSource(ids);
+  disabled_ = 0;
+}
+
+void SourceItem::disable() {
+  Database db;
+  QList<int> ids;
+  ids << id_;
+  db.disableSource(ids);
+  disabled_ = 1;
+}
+
+SourceModel::SourceModel(QObject* parent) : QAbstractTableModel(parent) {}
+
+SourceModel::~SourceModel() {
+  for (SourceItem* item : this->items) delete item;
+}
+
+QVariant SourceModel::headerData(int section, Qt::Orientation orientation,
+                                 int role) const {
+  if (role == Qt::DisplayRole) {
+    if (orientation == Qt::Horizontal) {
+      switch (section) {
+        case 0:
+          return QString("Name");
+        case 1:
+          return QString("#");
+        case 2:
+          return QString("results");
+        case 3:
+          return QString("wpm");
+        case 4:
+          return QString("dis");
+      }
+    }
+  }
+  return QVariant();
+}
+
+int SourceModel::rowCount(const QModelIndex& parent) const {
+  return this->items.size();
+}
+
+int SourceModel::columnCount(const QModelIndex& parent) const { return 5; }
+
+QVariant SourceModel::data(const QModelIndex& index, int role) const {
+  int row = index.row();
+  int column = index.column();
+  SourceItem* item = this->items[row];
+
+  if (role == Qt::DisplayRole) {
+    switch (column) {
+      case 0:
+        return item->name_;
+      case 1:
+        return item->text_count_;
+      case 2:
+        return item->results_;
+      case 3:
+        return item->wpm_;
+      case 4:
+        return item->disabled_;
+    }
+  } else if (role == Qt::UserRole) {
+    return item->id_;
+  }
+  return QVariant();
+}
+
+void SourceModel::refresh() {
+  this->clear();
+
+  Database db;
+  QList<QVariantList> rows = db.getSourcesData();
+  this->beginInsertRows(QModelIndex(), this->rowCount(),
+                        this->rowCount() + rows.size() - 1);
+  for (const auto& row : rows) {
+    SourceItem* item =
+        new SourceItem(row[0].toInt(), row[1].toString(), row[5].toInt(), 0, 0,
+                       row[2].toInt(), row[3].toInt(), row[4].toDouble());
+    this->items.append(item);
+  }
+  this->endInsertRows();
+}
+
+void SourceModel::refreshSource(const QModelIndex& index) {
+  if (!index.isValid()) return;
+
+  SourceItem* item = this->items[index.row()];
+  item->refresh();
+  emit dataChanged(index, this->index(index.row(), this->columnCount()));
+}
+
+void SourceModel::refreshSource(int source) {
+  for (int row = 0; row < this->rowCount(); row++) {
+    auto index = this->index(row, 0);
+    if (index.data(Qt::UserRole) == source) {
+      this->items[row]->refresh();
+      emit dataChanged(index, this->index(index.row(), this->columnCount()));
+      return;
+    }
+  }
+}
+
+void SourceModel::removeIndexes(const QModelIndexList& indexes) {
+  if (indexes.isEmpty()) return;
+
+  QList<int> rows;
+
+  for (const auto& index : indexes) rows << index.row();
+  qSort(rows.begin(), rows.end());
+
+  for (int i = rows.size() - 1; i >= 0; i--) {
+    int row = rows[i];
+    this->beginRemoveRows(QModelIndex(), row, row);
+    SourceItem* item = this->items.takeAt(row);
+    if (item) {
+      item->deleteFromDb();
+      delete item;
+    }
+    this->endRemoveRows();
+  }
+}
+
+void SourceModel::clear() {
+  this->beginResetModel();
+  for (SourceItem* item : this->items) delete item;
+  this->items.clear();
+  this->endResetModel();
+}
+
+void SourceModel::deleteSource(const QModelIndex& index) {
+  if (!index.isValid()) return;
+  this->beginRemoveRows(QModelIndex(), index.row(), index.row());
+  if (index.row() >= 0 && index.row() < this->items.size()) {
+    SourceItem* item = this->items.takeAt(index.row());
+    item->deleteFromDb();
+    delete item;
+  }
+  this->endRemoveRows();
+}
+
+void SourceModel::enableSource(const QModelIndex& index) {
+  if (index.row() >= 0 && index.row() < this->items.size()) {
+    this->items[index.row()]->enable();
+    emit dataChanged(index, this->index(index.row(), this->columnCount()));
+  }
+}
+
+void SourceModel::disableSource(const QModelIndex& index) {
+  if (index.row() >= 0 && index.row() < this->items.size()) {
+    this->items[index.row()]->disable();
+    emit dataChanged(index, this->index(index.row(), this->columnCount()));
+  }
+}
+
+void SourceModel::addSource(const QString& name) {
+  Database db;
+  db.getSource(name);
+  this->refresh();
+}
---
+++ src/texts/sourcemodel.h
@@ -0,0 +1,58 @@
+#ifndef SRC_TEXTS_SOURCEMODEL_H_
+#define SRC_TEXTS_SOURCEMODEL_H_
+
+#include <QAbstractTableModel>
+#include <QModelIndex>
+#include <QObject>
+#include <QVector>
+#include <QVariant>
+
+class SourceItem {
+  friend class SourceModel;
+
+ public:
+  SourceItem(int, const QString &, int, int, int, int, int, double);
+  int id() { return id_; };
+  void refresh();
+  void deleteFromDb();
+  void enable();
+  void disable();
+
+ private:
+  int id_;
+  QString name_;
+  int disabled_;
+  int discount_;
+  int type_;
+  int text_count_;
+  int results_;
+  double wpm_;
+};
+
+class SourceModel : public QAbstractTableModel {
+  Q_OBJECT
+
+ public:
+  explicit SourceModel(QObject *parent = Q_NULLPTR);
+  ~SourceModel();
+
+  void clear();
+  void refresh();
+  QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+  int rowCount(const QModelIndex &parent = QModelIndex()) const;
+  int columnCount(const QModelIndex &parent = QModelIndex()) const;
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+  void removeIndexes(const QModelIndexList &indexes);
+  void refreshSource(const QModelIndex& index);
+  void refreshSource(int source);
+  void deleteSource(const QModelIndex &index);
+  void enableSource(const QModelIndex &index);
+  void disableSource(const QModelIndex &index);
+  void addSource(const QString& name);
+
+ private:
+  QVector<SourceItem *> items;
+};
+
+#endif  // SRC_TEXTS_SOURCEMODEL_H_
