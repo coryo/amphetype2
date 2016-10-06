58b3f8f8d7bdfd60fc01cfb6f3f126516cbab291
Cory Parsons <babylonstudio@gmail.com>
2016-10-05 20:34:42 -0400

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,9 +9,9 @@ else
   mkdir dist
   cp bundle/linux/* dist/
   mv src/amphetype2 dist/
-  sudo wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage" -O /usr/local/bin/linuxdeployqt
-  sudo chmod a+x /usr/local/bin/linuxdeployqt
-  linuxdeployqt dist/amphetype2 -appimage -bundle-non-qt-libs -verbose=2
+  wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage" -O linuxdeployqt.AppImage
+  chmod a+x linuxdeployqt.AppImage
+  ./linuxdeployqt.AppImage dist/amphetype2 -appimage -bundle-non-qt-libs -verbose=2
   mv dist.AppImage amphetype2.AppImage
   tar -czf amphetype2-linux-x86_64-AppImage.tar.gz amphetype2.AppImage txt/
   export DEPLOYFILE=amphetype2-linux-x86_64-AppImage.tar.gz
