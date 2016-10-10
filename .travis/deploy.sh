765cf1f8a049371278bcce3246d01b00024247ba
Cory Parsons <babylonstudio@gmail.com>
2016-10-10 19:51:20 -0400

    dont bundle txt

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,8 +5,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   hdiutil mount macdeployqt.dmg
   sudo /Volumes/*macdeployqt*/macdeployqt.app/Contents/MacOS/macdeployqt src/amphetype2.app
   mv src/amphetype2.app .
-  tar -czf amphetype2-osx.tar.gz amphetype2.app txt/
-  export DEPLOYFILE=amphetype2-osx.tar.gz
+  # tar -czf amphetype2-osx.tar.gz amphetype2.app txt/
+  export DEPLOYFILE=amphetype2.app
 else
   mkdir dist
   cp bundle/linux/* dist/
@@ -14,7 +14,7 @@ else
   wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage" -O linuxdeployqt.AppImage
   chmod a+x linuxdeployqt.AppImage
   ./linuxdeployqt.AppImage dist/amphetype2 -appimage -bundle-non-qt-libs -verbose=2
-  mv dist.AppImage amphetype2.AppImage
-  tar -czf amphetype2-linux-x86_64-AppImage.tar.gz amphetype2.AppImage txt/
-  export DEPLOYFILE=amphetype2-linux-x86_64-AppImage.tar.gz
+  mv dist.AppImage amphetype2-linux-x86_64.AppImage
+  # tar -czf amphetype2-linux-x86_64-AppImage.tar.gz amphetype2.AppImage txt/
+  export DEPLOYFILE=amphetype2-linux-x86_64.AppImage
 fi
