2d41de3b0c19ab9286672ff0d6a7c11eadc13d9c
Cory Parsons <babylonstudio@gmail.com>
2016-10-14 12:36:08 -0400

    osx

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  # curl -L -O https://github.com/MaximAlien/macdeployqt/raw/master/macdeployqt.dmg
-  # hdiutil mount macdeployqt.dmg
-  # sudo /Volumes/*macdeployqt*/macdeployqt.app/Contents/MacOS/macdeployqt src/amphetype2.app -dmg
+  sudo chmod +x ./tools/macdeployqt.app/Contents/MacOS/macdeployqt
   sudo ./tools/macdeployqt.app/Contents/MacOS/macdeployqt src/amphetype2.app -dmg
   user=$(id -nu)
   sudo chown ${user} src/amphetype2.dmg
@@ -13,7 +11,6 @@ else
   mkdir dist
   cp bundle/linux/* dist/
   mv src/amphetype2 dist/
-  # wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage" -O linuxdeployqt.AppImage
   chmod a+x tools/linuxdeployqt.AppImage
   ./tools/linuxdeployqt.AppImage dist/amphetype2 -appimage -bundle-non-qt-libs -verbose=2
   mv dist.AppImage amphetype2-linux-x86_64.AppImage
